acd521284f06d37e8685b2684a7268b768a74bfb
Gangadhar <gangadharhosamani050@gmail.com>
2026-06-15 23:03:14 +0530

    update security

--- src/main/java/com/visitor_x/enums/PurposeOfVisit.java
+++ src/main/java/com/visitor_x/enums/PurposeOfVisit.java
@@ -1,33 +1,40 @@
 package com.visitor_x.enums;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum PurposeOfVisit {
-
-    INTERVIEW("Interview"),
-    INTERNSHIP("Internship"),
-    FULL_TIME_EMPLOYEE("Full_Time_Employee");
-
-    private final String label;
-
-    PurposeOfVisit(String label) {
-        this.label = label;
-    }
-
-    @JsonValue
-    public String getLabel() {
-        return label;
-    }
-
-    @JsonCreator
-    public static PurposeOfVisit fromValue(String value) {
-        for (PurposeOfVisit purpose : PurposeOfVisit.values()) {
-            if (purpose.name().equalsIgnoreCase(value) ||
-                    purpose.label.equalsIgnoreCase(value)) {
-                return purpose;
-            }
-        }
-        throw new IllegalArgumentException("Invalid purpose of visit: " + value);
-    }
+    INTERVIEW,
+    INTERNSHIP,
+    FULL_TIME_EMPLOYMENT
 }
+
+//import com.fasterxml.jackson.annotation.JsonCreator;
+//import com.fasterxml.jackson.annotation.JsonValue;
+//
+//public enum PurposeOfVisit {
+//
+//    INTERVIEW("Interview"),
+//    INTERNSHIP("Internship"),
+//    FULL_TIME_EMPLOYEE("Full_Time_Employement");
+//
+//    private final String label;
+//
+//    PurposeOfVisit(String label) {
+//        this.label = label;
+//    }
+//
+//    @JsonValue
+//    public String getLabel() {
+//        return label;
+//    }
+//
+//    @JsonCreator
+//    public static PurposeOfVisit fromValue(String value) {
+//        for (PurposeOfVisit purpose : PurposeOfVisit.values()) {
+//            if (purpose.name().equalsIgnoreCase(value) ||
+//                    purpose.label.equalsIgnoreCase(value)) {
+//                return purpose;
+//            }
+//        }
+//        throw new IllegalArgumentException("Invalid purpose of visit: " + value);
+//    }
+//}
